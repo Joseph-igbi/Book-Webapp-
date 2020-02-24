d9e4df329b53238d39f1370d5a04c3c944997fc4
Joseph-Igbi <Joseph.Igbinadolor@qa.com>
2020-02-24 10:46:48 +0000

    jenkins again

--- newMyFlask/provision.sh
+++ newMyFlask/provision.sh
@@ -2,6 +2,6 @@
 
 #echo export PROJ_DB_URI="mysql+pymysql://root:admin@34.89.33.37/flask1" >> /.bashrc
 
-export PROJ_DB_URI="mysql+pymysql://root:admin@34.89.33.37/flask1" 
-export MY_SECRET_KEY="7218a9143c27c16610765205a1b21cb7" 
+export DATABASE_URI="mysql+pymysql://root:admin@34.89.33.37/flask1" 
+export SECRET_KEY="7218a9143c27c16610765205a1b21cb7" 
 #echo export MY_SECRET_KEY="7218a9143c27c16610765205a1b21cb7" >> /.bashrc
