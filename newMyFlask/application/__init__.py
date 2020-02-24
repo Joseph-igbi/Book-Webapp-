e798fb9f4eed835d8d0ea4be77707ec080db0367
Joseph-Igbi <Joseph.Igbinadolor@qa.com>
2020-02-24 10:59:10 +0000

    jenkins changes

--- newMyFlask/application/__init__.py
+++ newMyFlask/application/__init__.py
@@ -7,14 +7,13 @@ from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']= os.getenv('DATABASE_URI')
+db = SQLAlchemy(app)
+from os import getenvapp.config['SECRET_KEY']= getenv('SECRET_KEY')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
-
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 bootstrap = Bootstrap(app)
 
-db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
