afd40d9eb1b01768527ceb5efdd0fcbc987ef854
Ubuntu <ubuntu@ip-172-31-17-241.eu-west-2.compute.internal>
2020-11-16 23:33:03 +0000

    var 4

--- newMyFlask/shell_variables.sh
+++ newMyFlask/shell_variables.sh
@@ -6,7 +6,7 @@ for i in DATABASE_URI2 MYSQL_DATABASE MYSQL_ROOT_PASSWORD SECRET_KEY
 
 do
 	#echo $(aws ssm get-parameters --name=$i --region=${aws_region} --output text --query "Parameters[0]"."Value");
-	echo $i=$(aws ssm get-parameters --name=$i --region=${aws_region} --output text --query "Parameters[0]"."Value") >> ~/.bashrc;
+	echo export $i=$(aws ssm get-parameters --name=$i --region=${aws_region} --output text --query "Parameters[0]"."Value");
 
 
 done	
